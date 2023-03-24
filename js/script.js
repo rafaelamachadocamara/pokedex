32e758fcbe0570c56b7322786a49c21757e1b1b1
Rafaela Machado <80546287+rafaelamachadocamara@users.noreply.github.com>
2023-03-24 00:24:24 +0000

    finishing pokedex

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1;
 
 
 const fetchPokemon = async (pokemon) => {
@@ -16,8 +19,12 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
-    
+
     if (data) {
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
@@ -36,4 +43,18 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     renderPokemon(input.value.toLowerCase());
 });
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+      searchPokemon -= 1;
+      renderPokemon(searchPokemon);
+    }
+  });
+  
+  buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  });
+
+  renderPokemon(searchPokemon);
 
